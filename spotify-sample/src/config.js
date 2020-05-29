c5a8b58d3fe52916b2630e34bdc11c7c6b8f9b3f
ar <56378858+arumbaua366@users.noreply.github.com>
2020-05-28 17:31:10 -0700

    add app and player details, updated config file

---
+++ spotify-sample/src/config.js
@@ -0,0 +1,11 @@
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+
+
+// Replace with your app's client ID, redirect URI and desired scopes
+export const clientId = "066fb3b4a6d149918cba6c91af463e9c";
+export const redirectUri = "http://localhost:3001/";
+export const scopes = [
+    "user-top-read",
+    "user-read-currently-playing",
+    "user-read-playback-state",
+];
